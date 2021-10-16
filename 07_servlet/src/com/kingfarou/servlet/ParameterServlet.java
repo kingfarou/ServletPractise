a8b05e5cbf6b3992b170908ada6ccda6815f7ba1
Kingfar Ou <kingfarou@163.com>
2021-10-16 16:24:12 +0800

    一些优化

--- 07_servlet/src/com/kingfarou/servlet/ParameterServlet.java
+++ 07_servlet/src/com/kingfarou/servlet/ParameterServlet.java
@@ -5,20 +5,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ParameterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //不要直接访问这个servlet，要通过form.html来跳转
+        req.setCharacterEncoding("UTF-8");
         System.out.println("用户名：" + req.getParameter("username"));
         System.out.println("密码->" + req.getParameter("password"));
         String[] hobbys = req.getParameterValues("hobby");
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(hobbys[0]);
-        for(int i = 1; i < hobbys.length; i++){
-            stringBuilder.append("、");
-            stringBuilder.append(hobbys[i]);
-        }
-        System.out.println("兴趣爱好：" + stringBuilder.toString());
+        System.out.println("兴趣爱好：" + Arrays.asList(hobbys));
     }
 }
