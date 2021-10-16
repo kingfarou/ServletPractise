5c3a5f9dc12e3d9f3138b99b4c1b0aa312dd557a
Kingfar Ou <kingfarou@163.com>
2021-10-16 16:22:30 +0800

    ServletContext的演示

--- 06_servlet/src/com/kingfarou/servlet/ContextServlet.java
+++ 06_servlet/src/com/kingfarou/servlet/ContextServlet.java
@@ -1,5 +1,6 @@
 package com.kingfarou.servlet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,11 +8,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ContextServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext servletContext = getServletConfig().getServletContext();
+        System.out.println("context的username->" + servletContext.getInitParameter("username"));
+        System.out.println("context的password->" + servletContext.getInitParameter("password"));
 
+        System.out.println("工程路径：" + servletContext.getContextPath());
+
+        System.out.println("工程部署在服务器的绝对路径：" + servletContext.getRealPath("/"));
     }
 }
